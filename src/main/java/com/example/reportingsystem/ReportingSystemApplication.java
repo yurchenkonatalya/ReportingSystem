ec52a6fa356c152e8566bd1cac67972a42f3803d
yurchenko <sckrepp@gmail.com>
2022-07-21 14:17:24 +0300

    Initial commit

---
+++ src/main/java/com/example/reportingsystem/ReportingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.reportingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReportingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/reportingsystem/ReportingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reportingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
